110aca4f1298eefc35ac69ed02212fe47f06c248
Ssethika <cristalls0406@gmail.com>
2024-06-27 00:10:40 +0200

    changes added

--- src/inputListener.cpp
+++ src/inputListener.cpp
@@ -1,13 +1,14 @@
+
 #include "inputListener.hpp"
 #include <SDL_events.h>
 
-const u8 InputListener::SetKeyboardState(int* numkeys){
-		_instance.currentKeyboardState = *SDL_GetKeyboardState(numkeys);
-		const u8 value = _instance.currentKeyboardState;
-    return value;
-	}
-bool InputListener::IsActionPressed(const char * action) { 
-		return std::string(GetCurrentKeyPressed(*GetEvent())) == std::string(PlayerSettings::GetKeybindings(action));
-	}
+const u8 InputListener::SetKeyboardState(int *numkeys) {
+  _instance.currentKeyboardState = *SDL_GetKeyboardState(numkeys);
+  const u8 value = _instance.currentKeyboardState;
+  return value;
+}
+bool InputListener::IsActionPressed(const char *action) {
+  return std::string(GetCurrentKeyPressed(*GetEvent())) ==
+         std::string(PlayerSettings::GetKeybindings(action));
+}
 InputListener InputListener::_instance;
-
